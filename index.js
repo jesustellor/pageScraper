d3ead877639cdcd46f51a95e28b901c840424c5d
jesustellor <jesustellor@gmail.com>
2024-09-19 17:43:22 +0000

    scrapping bbc.com for links that contain the word watch

--- index.js
+++ index.js
@@ -4,7 +4,34 @@ const app = express();
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+app.get('/', (req, res) => {
+    res.json("Welcome to my API")
+})
+
+const articles = [];
+
+app.get('/news', (req, res) => {
+    axios.get('https://www.bbc.com/news')
+    .then((response) => {
+        const html = response.data;
+        console.log(html);
+        const $ = cheerio.load(html);
+        const articles = [];
+        $('a:contains("Watch")', html).each(function() {
+            const text = $(this).text();
+            const url = $(this).attr('href');
+            articles.push({
+                text,
+                url
+            })
+        })
+        res.json(articles);
+    }).catch((err) => {
+        console.log(err)
+    });
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     
-})
+})  
